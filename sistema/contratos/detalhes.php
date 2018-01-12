480cf92b0236cffc9e167d4b30a7b3cec29ffdd0
Judson Bandeira <judson.bandeira@cge.al.gov.br>
2018-01-12 12:14:23 -0200

    Finalização de ajustes em Contratos

--- sistema/contratos/detalhes.php
+++ sistema/contratos/detalhes.php
@@ -17,9 +17,9 @@ $informacoes = retorna_informacoes($tabela, $id, $conexao_com_banco);
 	</div>
 	
 	<div class="container caixa-conteudo">
-	<div id="uatualizacao">
-		<span style="color: red;">Última atualização por <?php echo retorna_nome_servidor($informacoes["ID_SERVIDOR_ATUALIZOU"], $conexao_com_banco) ?> em <?php echo date_format(new DateTime($informacoes["DT_ULTIMA_ATUALIZACAO"]), 'd/m/Y') ?></span>
-	</div>
+		<div id="uatualizacao">
+			<span style="color: red;">Última atualização por <?php echo retorna_nome_servidor($informacoes["ID_SERVIDOR_ATUALIZOU"], $conexao_com_banco) ?> em <?php echo date_format(new DateTime($informacoes["DT_ULTIMA_ATUALIZACAO"]), 'd/m/Y') ?></span>
+		</div>
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="container">
@@ -28,40 +28,83 @@ $informacoes = retorna_informacoes($tabela, $id, $conexao_com_banco);
 							
 							<h2>INFORMAÇÕES</h2>
 							<hr>
-							<b>Contratado</b>:           <?php echo retorna_nome_empresa($informacoes["ID_EMPRESA"], $conexao_com_banco) ?><br>
-							<b>CNPJ</b>:                 <?php echo retorna_cnpj_empresa($informacoes["ID_EMPRESA"], $conexao_com_banco) ?><br>
-							<b>Órgão</b>:                   <?php echo retorna_nome_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?> - <?php echo retorna_sigla_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?> <br>
-							<b>UG</b>:                      <?php echo retorna_ug_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?><br>
-							<b>Gestor do contrato</b>:      <?php echo retorna_nome_servidor($informacoes["ID_SERVIDOR_GESTOR"], $conexao_com_banco) ?><br>
-							<b>Objeto do contrato</b>:      <?php echo $informacoes["NM_OBJETO"] ?><br>
-							<b>Modalidade</b>:              <?php echo $informacoes["NM_MODALIDADE"] ?><br>
-							<b>Processo administrativo</b>: <?php echo $informacoes["NM_PROCESSO"] ?><br>
-							<b>Vinculação</b>:              <?php echo $informacoes["NM_VINCULACAO"] ?><br>
+							<b>Contratado</b>: 
+								<?php echo retorna_nome_empresa($informacoes["ID_EMPRESA"],$conexao_com_banco) ?>
+								<br>
+							<b>CNPJ</b>:
+								<?php echo retorna_cnpj_empresa($informacoes["ID_EMPRESA"], $conexao_com_banco) ?>
+								<br>
+							<b>Órgão</b>:
+								<?php echo retorna_nome_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?> - <?php echo retorna_sigla_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?>
+								<br>
+							<b>UG</b>:
+								<?php echo retorna_ug_orgao($informacoes["ID_ORGAO"], $conexao_com_banco) ?>
+								<br>
+							<b>Gestor do contrato</b>:
+								<?php echo retorna_nome_servidor($informacoes["ID_SERVIDOR_GESTOR"], $conexao_com_banco) ?><br>
+							<b>Objeto do contrato</b>:
+								<?php echo $informacoes["NM_OBJETO"] ?>
+								<br>
+							<b>Modalidade</b>:
+								<?php echo $informacoes["NM_MODALIDADE"] ?>
+								<br>
+							<b>Processo administrativo</b>:
+								<?php echo $informacoes["NM_PROCESSO"] ?>
+								<br>
+							<b>Vinculação</b>:
+								<?php echo $informacoes["NM_VINCULACAO"] ?>
+								<br>
 							<hr>
 							<h2>VIGÊNCIA</h2>
 							<hr>
-							<b>Data de assinatura</b>:      <?php echo date_format(new DateTime($informacoes["DT_ASSINATURA"]), 'd/m/Y') ?><br>
-							<b>Data de publicação</b>:      <?php echo date_format(new DateTime($informacoes["DT_PUBLICACAO"]), 'd/m/Y') ?><br>
-							<b>Data de início</b>:          <?php echo date_format(new DateTime($informacoes["DT_INICIO"]), 'd/m/Y') ?><br>
-							<b>Data de término</b>:         <?php echo date_format(new DateTime($informacoes["DT_TERMINO"]), 'd/m/Y') ?><br>
+							<b>Data de assinatura</b>:
+								<?php echo date_format(new DateTime($informacoes["DT_ASSINATURA"]), 'd/m/Y') ?>
+								<br>
+							<b>Data de publicação</b>:
+								<?php echo date_format(new DateTime($informacoes["DT_PUBLICACAO"]), 'd/m/Y') ?>
+								<br>
+							<b>Data de início</b>:
+								<?php echo date_format(new DateTime($informacoes["DT_INICIO"]), 'd/m/Y') ?>
+								<br>
+							<b>Data de término</b>:
+								<?php echo date_format(new DateTime($informacoes["DT_TERMINO"]), 'd/m/Y') ?>
+								<br>
 							<hr>
 							<h2>IDENTIFICAÇÃO</h2>
 							<hr>
-							<b>Número do contrato</b>:          <?php echo $informacoes["NM_NUMERO_CONTRATO"] ?><br>
-							<b>Número do contrato no SIAFI</b>: <?php echo $informacoes["NM_NUMERO_CONTRATO_SIAFI"] ?><br>
+							<b>Número do contrato</b>:
+								<?php echo $informacoes["NM_NUMERO_CONTRATO"] ?>
+								<br>
+							<b>Número do contrato no SIAFI</b>:
+								<?php echo $informacoes["NM_NUMERO_CONTRATO_SIAFI"] ?>
+								<br>
 							<hr>
 							<h2>VALOR</h2>
 							<hr>
-							<b>Valor global</b>:             <?php echo "R$ " . number_format($informacoes["VL_GLOBAL"],2, ",", ".") ?><br>
-							<b>Valor empenhado</b>:          <?php echo "R$ " . number_format($informacoes["VL_EMPENHADO"],2, ",", ".") ?><br>
-							<b>Valor liquidado</b>:          <?php echo "R$ " . number_format($informacoes["VL_LIQUIDADO"],2, ",", ".") ?><br>
-							<b>Valor pago até o momento</b>: <?php echo "R$ " . number_format($informacoes["VL_PAGO"],2, ",", ".") ?><br>
-							<b>Diferença</b>:                <?php echo "R$ " . number_format($informacoes["VL_GLOBAL"]-$informacoes["VL_PAGO"],2, ",", ".") ?><br>
+							<b>Valor global</b>:
+								<?php echo "R$ " . number_format($informacoes["VL_GLOBAL"],2, ",", ".") ?>
+								<br>
+							<b>Valor empenhado</b>:
+								<?php echo "R$ " . number_format($informacoes["VL_EMPENHADO"],2, ",", ".") ?>
+								<br>
+							<b>Valor liquidado</b>:
+								<?php echo "R$ " . number_format($informacoes["VL_LIQUIDADO"],2, ",", ".") ?>
+								<br>
+							<b>Valor pago até o momento</b>:
+								<?php echo "R$ " . number_format($informacoes["VL_PAGO"],2, ",", ".") ?>
+								<br>
+							<b>Diferença</b>:
+								<?php echo "R$ " . number_format($informacoes["VL_GLOBAL"]-$informacoes["VL_PAGO"],2, ",", ".") ?>
+								<br>
 							<hr>
 							<h2>OUTRAS INFORMAÇÕES</h2>
 							<hr>
-							<b>É Prorrogável?</b> <?php if($informacoes["BL_PRORROGAVEL"]){echo "Sim";}else{echo "Não";} ?><br>
-							<b>Termo aditivo</b>: <?php echo $informacoes["NR_TERMO_ADITIVO"] ?>º<br>
+							<b>É Prorrogável?</b>
+								<?php if($informacoes["BL_PRORROGAVEL"]){echo "Sim";}else{echo "Não";} ?>
+								<br>
+							<b>Termo aditivo</b>:
+								<?php echo $informacoes["NR_TERMO_ADITIVO"] ?>º
+								<br>
 						</div>
 					</div>
 					
